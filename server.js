97fed0ba38f6a26c5b0a5114d649a85f051ecb86
goojal <amiralimail@gmail.com>
2021-12-02 17:15:01 +0300

    add cors middleware needed by FCC

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({optionsSuccessStatus: 200}));
 
 app.get('/api/:date?', (req, res) => {
     let dateParam = req.params.date;
