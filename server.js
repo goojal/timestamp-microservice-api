a4684300e1cc34db812b44b7ed4f96dfcca6ad66
goojal <amiralimail@gmail.com>
2021-12-02 17:11:02 +0300

    handle empty param

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/api/:date?', (req, res) => {
-    let date_param = req.params.date;
-    if (!isNaN(date_param)) {
-        date_param = parseInt(date_param);
+    let dateParam = req.params.date;
+    if (!isNaN(dateParam)) {
+        dateParam = parseInt(dateParam);
     }
-    let d = new Date(date_param);
+    let d = new Date(dateParam);
+    if (dateParam === undefined) {
+        d = new Date();
+    }
+    
+    let data = { unix: d.getTime(), utc: d.toUTCString()};
     if (isNaN(d.getTime())) {
-        res.json({ error : "Invalid Date" });
+        data = { error : "Invalid Date" };
     }
-    res.json({ unix: d.getTime(), utc: d.toUTCString()});
+    res.json(data);
 });
 
 app.listen(port, () => {
